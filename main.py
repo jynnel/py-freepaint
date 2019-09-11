8ceceae5970169e75431535b42f45cbf9652c1eb
vrav <bcdburrow@gmail.com>
2019-09-11 15:24:36 -0700

    add if conditions to Xlib usage to make it run in Windows

--- main.py
+++ main.py
@@ -8,22 +8,26 @@ import sys
 import time
 
 from modules.sdlapp import SDLApp
-from modules.xdevices import XDevices
+
+if sys.platform.startswith("linux"):
+    from modules.xdevices import XDevices
 
 framerate = 120.0
 framedelta = 1.0 / framerate
 
 def main(argv):
     app = SDLApp("py-freepaint", 580, 580)
-    xd = XDevices()
 
-    found_stylus = xd.add_device("stylus")
+    found_stylus = False
+    if sys.platform.startswith("linux"):
+        xd = XDevices()
+        found_stylus = xd.add_device("stylus")
 
     waitpoint = time.time() + framedelta
     while app.running:
         app.parse_events()
         
-        if found_stylus:
+        if sys.platform.startswith("linux") and found_stylus:
             xd.update_devices()
             if xd.is_device_active("stylus"):
                 print("stylus", xd.get_device_values("stylus"))
